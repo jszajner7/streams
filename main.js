91f9903815c153b0bbcc87f00c72302cfe0ff128
jeff <jszajner@yahoo.com>
2016-10-09 02:46:12 +0000

    streams

---
+++ main.js
@@ -0,0 +1,19 @@
+var Alphabet = require('./alphabet'); //should this block of code stay?//
+var alpha = new Alphabet();
+alpha.on('data', function(chunk) {
+    console.log(chunk.toString());
+});
+
+var Alphabet = require('./alphabet');
+var Cache = require('.cache');
+var alpha =new Alphabet();
+var cache = new Cache('alpha1');
+
+alpha.pipe(cache);
+
+cache.on('finish' ,function() {
+    console.log('Cache store:');
+    for (var key in Cache.store) {
+        console.log(key, ':', Cache.store[key]);
+}
+});
